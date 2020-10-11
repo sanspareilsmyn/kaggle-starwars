ae230f6f74582616eb01d90df6f57420d7ceaeed
sanspareilsmyn <sanspareilsmyn@gmail.com>
2020-10-11 22:54:51 +0900

    201011

--- preprocessing.py
+++ preprocessing.py
@@ -1,23 +1,14 @@
-import snap
 import json
 
-#Graph = snap.PNGraph.new() # Undirected Network
 id_map = dict()
 inv_id_map = dict()
-link_map = dict()
 id_count = 0
-link_count = 0
 
 print("Reading json")
-with open('dataset/starwars-episode-1-interactions.json', 'r') as f:
+with open('dataset/starwars-full-interactions.json', 'r') as f:
     json_data = json.load(f)
-#print(json.dumps(json_data))
-#print(json.dumps(json_data, indent="\t"))
 
-#a = json_data["nodes"][0]["name"]
-#print(len(json_data["nodes"])) #37
-#print(len(json_data["links"])) #129
-
+print("----- Creating Interaction Edgelist -----")
 print("Creating name map")
 json_node = json_data["nodes"]
 for character in json_node:
@@ -25,20 +16,49 @@ for character in json_node:
     inv_id_map[id_count] = character["name"]
     id_count += 1
 
-#print(id_map)
-#print(inv_id_map)
-
-print("Creating link map")
+print("Creating edges with weight")
 json_link = json_data["links"]
 line = open('interaction_link_with_weight.edgelist', 'w')
 for character in json_link:
-    #print(character) #{'source': 1, 'target': 0, 'value': 1}
     start = character["source"]
     end = character["target"]
     weight = float(character["value"])
     line.write('{} {} {}\n'.format(start, end, weight))
-    line.write('{} {} {}\n'.format(end, start, weight))
+line.close()
+print('Interaction Edgelist is created!')
 
+#######################################################################
+print("----- Creating Mentions Edgelist -----")
 
+id_map_m_unordered = dict()
+inv_id_map_m_unordered = dict()
+id_map_m = dict()
+inv_id_map_m = dict()
+id_count_m = 0
+
+with open('dataset/starwars-full-mentions.json', 'r') as f:
+    json_data_m = json.load(f)
+print("Creating name map")
+json_node_m = json_data_m["nodes"]
+for character in json_node_m:
+    if character["name"] in id_map:
+        id_map_m[character["name"]] = id_map[character["name"]]
+        inv_id_map_m[id_map[character["name"]]] = character["name"]
+
+        id_map_m_unordered[character["name"]] = id_count_m
+        inv_id_map_m_unordered[id_count_m] = character["name"]
+        id_count_m += 1
+
+print("Creating edges with weight")
+json_link_m = json_data_m["links"]
+line = open('mentions_link_with_weight.edgelist', 'w')
+
+for character in json_link_m:
+    if character["source"] < len(id_map) and character["target"] < len(id_map):
+        start = id_map[inv_id_map_m_unordered[character["source"]]]
+        end = id_map[inv_id_map_m_unordered[character["target"]]]
+        weight = float(character["value"])
+        line.write('{} {} {}\n'.format(start, end, weight))
 line.close()
 
+print('Mentions Edgelist is created!')
